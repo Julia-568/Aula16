f090fd486e5ce0616b2d322befc5c18f7d699a67
Julia Souza <juju.souza09@yahoo.com>
2025-05-28 11:21:03 -0300

    Add files via upload

--- JuntaVetores.java
+++ JuntaVetores.java
@@ -6,29 +6,30 @@ public class JuntaVetores {
 	
 	public static void main(String[] args) {
 		
-		Scanner ler = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		
 		int vetor1 [] = new int[2];
 		int vetor2 [] = new int[2];
-		int vetoresJuntos [] = new int[4];
+		int resultado [] = new int[4];
 		
+		System.out.println("Leitura do primeiro array:");
 		for (int i = 0; i < 2; i++) {
 			System.out.println("Informe o " + i + "° valor: ");
-			vetor1[i] = ler.nextInt();
+			vetor1[i] = scanner.nextInt();
+			resultado[i] = vetor1[i];
 	}
+		System.out.println("Leitura do segundo array:");
 		for (int i = 0; i < 2; i++) {
 			System.out.println("Informe o " + i + "° valor: ");
-			vetor2[i] = ler.nextInt();
+			vetor2[i] = scanner.nextInt();
+			resultado[i+2] = vetor2[i];
 	}
-		for (int i = 0; i < 2; i++) {
-			vetoresJuntos[i] = vetor1[i];
-			vetoresJuntos[i+2] = vetor2[i];	
-		}
+		System.out.println("Array resultante:");
 		for (int i = 0; i < 4; i++) {
-			System.out.println("Informe o valor " + i + "o valor: " + vetoresJuntos[i]);
+			System.out.println("Informe o valor " + i + "o valor: " + resultado[i]);
 		}
 		
-		ler.close();
+		scanner.close();
 
     }
 }
